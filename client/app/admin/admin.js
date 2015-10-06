a7392b85ca8ba403c527556bb53669fb2344c26f
jaaimino <Jacobaimino@gmail.com>
2015-10-06 15:38:05 -0400

    Initial push

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('emoneyAdviseApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
